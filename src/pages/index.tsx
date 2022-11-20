3824e816636563099d12b1ddba1cbf1d2c0f5dd9
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2022-11-20 18:38:02 -0300

    Add link navigation and Product page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import { GetStaticProps } from "next";
-
+import Image from "next/image";
+import Link from "next/link";
+import Stripe from "stripe";
 import { useKeenSlider } from "keen-slider/react";
 
 import { stripe } from "../lib/stripe";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import "keen-slider/keen-slider.min.css";
-import Stripe from "stripe";
 
 interface HomeProps {
   products: {
@@ -30,13 +30,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product className="keen-slider__slide" key={product.id}>
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         );
       })}
     </HomeContainer>
